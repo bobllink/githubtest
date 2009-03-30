2b0b9e4a505d604e16b251c72d7bcfbca2822145
Bob Liverman <bobllink@gmail.com>
2009-03-29 23:20:22 -0400

    find .gitignore that supports cocoa xcode compile after github clone

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GitTestCase
+//
+//  Created by Bob Liverman on 3/29/09.
+//  Copyright My Company 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
